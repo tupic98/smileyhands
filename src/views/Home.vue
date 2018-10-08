28a747ca16ab046c5d8ef238a2b3958a334fc31c
RodrigoA <00017316@uca.edu.sv>
2018-10-08 02:14:02 -0600

    Added som images to gallery

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="rooty">
     <div class="parallax">
+      <h1 class="introduction">Introducción</h1>
     </div>
      <v-layout
       align-center
@@ -8,22 +9,75 @@
       justify-center class="protocolos">
         <h1 class="display-2 font-weight-thin mb-3">Protocolos</h1>
         <hr class="line">
+        <div class="protocols__buttons">
+      <div class="button__container">
+          <img src="@/assets/protocol.svg" class="prtocol_img" alt="">
+          <h3>Como tratar a un niño con discapacidad auditiva.</h3>
+          <br>
+          <p>Recomendaciones apropiadas especialmente para niños con discapacidad auditiva.</p>
+      </div>
+      <div class="button__container">
+          <img src="@/assets/protocol.svg" class="prtocol_img" alt="">
+          <h3>Como tratar a un adulto con discapacidad auditiva.</h3>
+          <br>
+          <p>Recomendaciones apropiadas personas con discapacidad auditiva.</p>
+      </div>
+      <div class="button__container">
+          <img src="@/assets/protocol.svg" class="prtocol_img" alt="">
+          <h3>Manifestaciones orales en un paciente con discapacidad auditiva.</h3>
+          <p>El paciente con discapacidad auditiva presenta alteraciones características.</p>
+      </div>
+    </div>
     </v-layout>
-    <div class="protocols__buttons">
-      <div class="button__container">
-          <img src="@/assets/protocol.svg" class="prtocol_img" alt="">
-          <h3>Protocolo 1</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi repellendus ad et placeat quod consectetur, architecto quidem voluptatum.</p>
+    
+    <div class="abecedario_container">
+      <div class="gallery__image">
+        <img src="@/assets/a.png" alt="" class="gallery__img">
+        <div class="middle">
+           <div class="text">A</div>
+        </div>
       </div>
-      <div class="button__container">
-          <img src="@/assets/protocol.svg" class="prtocol_img" alt="">
-          <h3>Protocolo 2</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi repellendus ad et placeat quod consectetur, architecto quidem voluptatum.</p>
+      <div class="gallery__image">
+        <img src="@/assets/b.png" alt="" class="gallery__img">
+        <div class="middle">
+           <div class="text">B</div>
+        </div>
       </div>
-      <div class="button__container">
-          <img src="@/assets/protocol.svg" class="prtocol_img" alt="">
-          <h3>Protocolo 3</h3>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi repellendus ad et placeat quod consectetur, architecto quidem voluptatum.</p>
+      <div class="gallery__image">
+        <img src="@/assets/c.png" alt="" class="gallery__img">
+        <div class="middle">
+           <div class="text">C</div>
+        </div>
+      </div>
+      <div class="gallery__image">
+        <img src="@/assets/d.png" alt="" class="gallery__img">
+        <div class="middle">
+           <div class="text">D</div>
+        </div>
+      </div>
+      <div class="gallery__image">
+        <img src="@/assets/e.png" alt="" class="gallery__img">
+        <div class="middle">
+           <div class="text">E</div>
+        </div>
+      </div>
+      <div class="gallery__image">
+        <img src="@/assets/f.png" alt="" class="gallery__img">
+        <div class="middle">
+           <div class="text">F</div>
+        </div>
+      </div>
+      <div class="gallery__image">
+        <img src="@/assets/g.png" alt="" class="gallery__img">
+        <div class="middle">
+           <div class="text">G</div>
+        </div>
+      </div>
+      <div class="gallery__image">
+        <img src="@/assets/h.png" alt="" class="gallery__img">
+        <div class="middle">
+           <div class="text">H</div>
+        </div>
       </div>
     </div>
   </div>
